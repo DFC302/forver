a7f254b9f9412ea7ab3239d463b6e90042cc7b32
Matthew Greer <mgreer906@gmail.com>
2020-05-08 22:21:50 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name='forver',
+	version='1.0.0',
+	author="Matthew Greer",
+	author_email="pydev302@gmail.com",
+        license='MIT',
+	description="A tool to perform quick forward and reverse DNS look-ups.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/DFC302/ctd",
+        keywords=['DNS', 'forward', 'reverse', 'ip', 'domain', 'lookup'],
+	packages=setuptools.find_packages(),
+        install_requires=[
+            "argparse"
+        ],
+        package_data={'': ['LICENSE'], '': ['README.md']},
+        include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+     ],
+     entry_points={
+            'console_scripts': [
+                "forver = main.forver:main",
+            ],
+        },
+)
